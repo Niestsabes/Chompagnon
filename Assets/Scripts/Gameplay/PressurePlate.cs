6a28a0d9f2e45d82cfc76e5d1e592fa417397534
Oramy <amaury.butaux@outlook.fr>
2023-05-07 09:59:12 +0200

    updates

---
+++ Assets/Scripts/Gameplay/PressurePlate.cs
@@ -0,0 +1,62 @@
+using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+
+    public UnityEvent eventTriggerEnter;
+    public UnityEvent eventTriggerExit;
+    public UnityEvent eventTriggerStayPushed;
+    public string tag = "Player";
+    public bool shouldBeGrounded = false;
+    private bool stayPushed = false;
+    private int pushed = 0;
+
+    public void StayPushed() {
+        stayPushed = true;
+        if (pushed == 0)
+            eventTriggerEnter.Invoke();
+        eventTriggerStayPushed.Invoke();
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (stayPushed)
+            return;
+        if (collision.gameObject.CompareTag(tag)) {
+            if (collision.gameObject.TryGetComponent<PlayerController>(out PlayerController playerController)) {
+                if (playerController.IsGrounded || !shouldBeGrounded) {
+                    if (pushed == 0)
+                        eventTriggerEnter.Invoke();
+                    pushed++;
+                }
+            }
+            else {
+                if (pushed == 0)
+                    eventTriggerEnter.Invoke();
+                pushed++;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (stayPushed)
+            return;
+        if (collision.gameObject.CompareTag(tag)) {
+            if (pushed == 1)
+                eventTriggerExit.Invoke();
+            pushed--;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PressurePlateGroup.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlateGroup : MonoBehaviour
+{
+    private PressurePlate[] pressurePlate;
+    private int pushedPlates = 0;
+    public UnityEvent onAllPushed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressurePlate = GetComponentsInChildren<PressurePlate>();
+        foreach (PressurePlate pressurePlate in pressurePlate) {
+            pressurePlate.eventTriggerEnter.AddListener(PushOne);
+            pressurePlate.eventTriggerExit.AddListener(UnpushOne);
+        }
+    }
+
+    public void PushOne() {
+        pushedPlates++;
+
+        if (pushedPlates == pressurePlate.Length) {
+            foreach (PressurePlate pressurePlate in pressurePlate) {
+                pressurePlate.StayPushed();
+            }
+            onAllPushed.Invoke();
+        }
+    }
+
+    public void UnpushOne() {
+        pushedPlates--; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
